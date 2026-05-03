f34da8317316af9700509e8c5f336a1674ec18be
syxanash <si.marzulli@gmail.com>
2026-05-03 17:19:47 +0100

    improved struct

--- apps/plugins/aapippo.c
+++ apps/plugins/aapippo.c
@@ -2,25 +2,46 @@
 
 enum plugin_status plugin_start(const void *parameter)
 {
+  // MARK: Setup
+
   (void)parameter;
   int btn;
-  int x = rb->rand() % (LCD_WIDTH - 8) + 4, y = rb->rand() % (LCD_HEIGHT - 8) + 4;
 
   rb->lcd_setfont(FONT_SYSFIXED);
 
-  bool x_verse = true;
-  bool y_verse = true;
-  bool bounced_y = false;
-  bool bounced_x = false;
-  int def_pixel = 8;
-  int splat_pixel = 15;
-  int pixel_width = 0;
-  int pixel_height = 0;
+  typedef struct
+  {
+    int x;
+    int y;
+    bool x_verse;
+    bool y_verse;
+    bool bounced_y;
+    bool bounced_x;
+    int def_pixel;
+    int splat_pixel;
+    int pixel_width;
+    int pixel_height;
+  } Blue;
+
+  Blue pippo = {
+      .x = rb->rand() % (LCD_WIDTH - 8) + 4,
+      .y = rb->rand() % (LCD_HEIGHT - 8) + 4,
+      .x_verse = true,
+      .y_verse = true,
+      .bounced_y = false,
+      .bounced_x = false,
+      .def_pixel = 8,
+      .splat_pixel = 15,
+      .pixel_width = 0,
+      .pixel_height = 0,
+  };
+
+  // MARK: Update
 
   while (true)
   {
-    bounced_x = false;
-    bounced_y = false;
+    pippo.bounced_x = false;
+    pippo.bounced_y = false;
 
     btn = rb->button_get(true);
 
@@ -29,76 +50,84 @@ enum plugin_status plugin_start(const void *parameter)
 
     if (btn == BUTTON_SCROLL_FWD)
     {
-      x_verse = !x_verse;
-      y_verse = !y_verse;
+      pippo.x_verse = !pippo.x_verse;
+      pippo.y_verse = !pippo.y_verse;
     }
 
     if (btn == BUTTON_SCROLL_BACK)
     {
-      x_verse = !x_verse;
-      y_verse = !y_verse;
+      pippo.x_verse = !pippo.x_verse;
+      pippo.y_verse = !pippo.y_verse;
     }
 
-    if (y_verse)
+    if (pippo.y_verse)
     {
-      y += 10;
+      pippo.y += 10;
     }
     else
     {
-      y -= 10;
+      pippo.y -= 10;
     }
 
-    if (x_verse)
+    if (pippo.x_verse)
     {
-      x += 10;
+      pippo.x += 10;
     }
     else
     {
-      x -= 10;
+      pippo.x -= 10;
     }
 
-    if ((y + 4) >= LCD_HEIGHT)
+    if ((pippo.y + 4) >= LCD_HEIGHT)
     {
-      bounced_y = true;
-      y_verse = false;
-      y = LCD_HEIGHT - 5;
+      pippo.bounced_y = true;
+      pippo.y_verse = false;
+      pippo.y = LCD_HEIGHT - 5;
     }
-    else if ((y - 4) <= 0)
+    else if ((pippo.y - 4) <= 0)
     {
-      bounced_y = true;
-      y_verse = true;
-      y = 4;
+      pippo.bounced_y = true;
+      pippo.y_verse = true;
+      pippo.y = 4;
     }
 
-    if ((x + 4) >= LCD_WIDTH)
+    if ((pippo.x + 4) >= LCD_WIDTH)
     {
-      bounced_x = true;
-      x_verse = false;
-      x = LCD_WIDTH - 5;
+      pippo.bounced_x = true;
+      pippo.x_verse = false;
+      pippo.x = LCD_WIDTH - 5;
     }
-    else if ((x - 4) <= 0)
+    else if ((pippo.x - 4) <= 0)
     {
-      bounced_x = true;
-      x_verse = true;
-      x = 4;
+      pippo.bounced_x = true;
+      pippo.x_verse = true;
+      pippo.x = 4;
     }
+
+    // MARK: Draw
 
     rb->lcd_clear_display();
     rb->lcd_set_foreground(LCD_RGBPACK(0, 200, 255));
 
-    if (bounced_x) {
-      pixel_height = splat_pixel;
-    } else {
-      pixel_height = def_pixel;
+    if (pippo.bounced_x)
+    {
+      pippo.pixel_height = pippo.splat_pixel;
     }
-    
-    if (bounced_y) {
-      pixel_width = splat_pixel;
-    } else {
-      pixel_width = def_pixel;
+    else
+    {
+      pippo.pixel_height = pippo.def_pixel;
     }
 
-    rb->lcd_fillrect(x - 4, y - 4, pixel_width, pixel_height);
+    if (pippo.bounced_y)
+    {
+      pippo.pixel_width = pippo.splat_pixel;
+    }
+    else
+    {
+      pippo.pixel_width = pippo.def_pixel;
+    }
+
+    rb->lcd_fillrect(pippo.x - 4, pippo.y - 4, pippo.pixel_width, pippo.pixel_height);
     rb->lcd_set_foreground(LCD_WHITE);
 
     rb->lcd_putsxy(0, 0, "MENU to quit");
